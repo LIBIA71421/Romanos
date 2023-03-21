9244e2a0343ba738864717146ec05b624b786295
libia <libia.roman@ucb.edu.bo>
2023-03-21 12:25:43 +0100

    1 en Romano

---
+++ src/romanos.js
@@ -0,0 +1,8 @@
+function romano(a) {
+    if (a == 1)
+    {
+      return "I"
+    }
+  }
+  
+  export default romano;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,15 @@
-import sumar from "./sumador.js";
+import romano from "./romanos.js";
+
+describe("Numero Romano", () => {
+  it("retorna el numero 1 en Romano", () => {
+    expect(romano(1)).toEqual("I");
+  });
+});
+
+/*import sumar from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
-});
+});*/
